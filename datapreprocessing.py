026cb49cc8b328f99c9f680dd73f1b75e42c67a7
Aashish1107 <aashishvennapusa1@gmail.com>
2025-11-01 19:19:01 -0500

    data retrival from csv file

--- datapreprocessing.py
+++ datapreprocessing.py
@@ -6,3 +6,13 @@ df = pd.read_csv(file_path)
 
 # Display the first few rows of the DataFrame
 print(df["moves"].head())
+
+df_moves = df[["white_rating", "black_rating", "moves"]].copy()
+df_moves["min_rating"] = df_moves[["white_rating", "black_rating"]].min(axis=1)
+df_moves = df_moves[["min_rating", "moves"]]
+
+# Display the resulting DataFrame
+print(df_moves.head())
+
+games=[[move for move in game.split(' ')] for game in df_moves["moves"]]
+print(games[0])
